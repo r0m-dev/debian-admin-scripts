0ff4f63225594d836c465bf87d06b5e6623ba535
1ceb0x <36591161+rberbille@users.noreply.github.com>
2018-04-08 18:23:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+echo Votre email ?
+read email
 apt update && apt full-upgrade -yy
 # Configure locale
 locale-gen --purge en_US.UTF-8
 echo -e 'LANG="fr_FR.UTF-8"\nLANGUAGE="fr_FR:fr"\n' > /etc/default/locale
 # Mail notification
-echo 'root: romain.berbille@gmail.com' > /etc/aliases
+echo 'root: $email' > /etc/aliases
 # Install pre-requisite
 apt install htop fail2ban net-tools nmap apt-transport-https dirmngr neofetch git build-essential unzip -yy
 apt install smartmontools -yy
