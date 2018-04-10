b0740ca0e00b75cdcb2513aa0846fb35b8bcc7d0
1ceb0x <36591161+rberbille@users.noreply.github.com>
2018-04-10 11:03:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ echo 'root: $email' > /etc/aliases
 apt install htop fail2ban net-tools nmap apt-transport-https dirmngr neofetch git build-essential unzip -yy
 apt install smartmontools -yy
 # Copy neofetch conf
-mv config/config-neofetch $HOME/.config/neofetch/config
+mkdir -p $HOME/.config/neofetch/config && mv config/config-neofetch $HOME/.config/neofetch/config
 # Install Docker
 echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list.d/docker.list
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
