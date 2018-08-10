697de77b9bbd55bc672d11420574f72d6721e228
Romain BERBILLE <romain.berbille@gmail.com>
2018-08-11 00:37:52 +0200

    update

--- setup.sh
+++ setup.sh
@@ -8,13 +8,22 @@ echo -e 'LANG="fr_FR.UTF-8"\nLANGUAGE="fr_FR:fr"\n' > /etc/default/locale
 # Mail notification
 echo 'root: $email' > /etc/aliases
 # Install pre-requisite
-apt install htop fail2ban net-tools nmap apt-transport-https dirmngr neofetch mc build-essential certbot unzip -yy
+apt install curl htop fail2ban net-tools nmap apt-transport-https dirmngr neofetch mc build-essential certbot unzip -yy
 apt install smartmontools -yy
+# Install nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install nodejs
 # Copy neofetch conf
 mkdir -p $HOME/.config/neofetch/ && mv config/config-neofetch $HOME/.config/neofetch/config
 # Purge /etc/motd
 echo '' > /etc/motd
 echo 'neofetch' >> /root/.bashrc
+# Desactivation IPV6
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.autoconf = 0' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.autoconf = 0' >> /etc/sysctl.conf
+sysctl -p
 # Autoriser le Root login via SSH
 sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 service sshd restart
