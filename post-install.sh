d9ff980cff831b4160b91659eea4bda5bf834cbb
Romain BERBILLE <romain.berbille@gmail.com>
2018-08-18 15:44:17 +0200

    update scripts 18082018

---
+++ post-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo Email de l'administrateur ?
+read email
+apt update && apt full-upgrade -yy
+# Configure locale
+locale-gen --purge en_US.UTF-8
+echo -e 'LANG="fr_FR.UTF-8"\nLANGUAGE="fr_FR:fr"\n' > /etc/default/locale
+# Mail notification
+echo 'root: $email' > /etc/aliases
+# Install pre-requisite
+apt install curl htop fail2ban net-tools nmap apt-transport-https dirmngr neofetch mc build-essential certbot unzip -yy
+apt install smartmontools -yy
+# Copy neofetch conf
+mkdir -p $HOME/.config/neofetch/ && mv config/config-neofetch $HOME/.config/neofetch/config
+# Purge /etc/motd
+echo '' > /etc/motd
+echo 'neofetch' >> /root/.bashrc
+# Desactivation IPV6
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.autoconf = 0' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.autoconf = 0' >> /etc/sysctl.conf
+sysctl -p
+# Autoriser le Root login via SSH
+sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
+service sshd restart
