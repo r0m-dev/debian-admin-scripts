819c82b7c62eec80839d124763545a690a30b4cb
Romain BERBILLE <36591161+rberbille@users.noreply.github.com>
2018-09-23 12:43:47 +0200

    Update webserver-install.sh

--- webserver-install.sh
+++ webserver-install.sh
@@ -17,6 +17,8 @@ mysql -uroot -p$rand -e 'update mysql.user set plugin="mysql_native_password";'
 mysql -uroot -p$rand -e "FLUSH PRIVILEGES;"
 # Installation de PHP 7.0
 apt install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring  php7.0-xml php7.0-zip -yy
+# Installation de Certbot auto pour apache
+apt install python-certbot-apache -yy
 clear
 echo "-------------------------------------------------------------"
 echo "Votre mot de passe MySQL est le suivant : "$rand
