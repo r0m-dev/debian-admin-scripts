cf1e324ffbf4ff3031b35e8caf341ad1440687b5
Romain BERBILLE <36591161+rberbille@users.noreply.github.com>
2018-09-28 17:36:48 +0200

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -2,5 +2,5 @@
 apt-get update -yy && apt-get upgrade -yy && apt-get install apt-transport-https -yy
 echo 'deb https://apt.dockerproject.org/repo debian-stretch main' >> /etc/apt/sources.list.d/docker.list
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-get install docker-engine -yy
+apt-get install docker-engine --allow-unauthenticated -yy
 
