f56369801745c34b115acf08217d85d4a345a243
Romain BERBILLE <36591161+rberbille@users.noreply.github.com>
2019-03-27 21:23:33 +0100

    Update nfs_server.sh

--- nfs_server.sh
+++ nfs_server.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-apt-get update && apt install nfs-kernel-server -yy
+apt-get update && apt install nfs-kernel-server -yy  
 
-echo '/data 192.168.17.0/24(rw,sync,no_subtree_check)' >> /etc/exports
+mkdir -p /srv/data  
 
-service nfs-kernel-server restart
+echo '/srv/data 192.168.17.0/24(rw,sync,no_subtree_check)' >> /etc/exports 
+
+service nfs-kernel-server restart  
