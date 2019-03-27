632d9236f116905aed48eba9602e8a252b9218e9
Romain BERBILLE <36591161+rberbille@users.noreply.github.com>
2019-03-27 21:01:47 +0100

    Update and rename dhcp_server to dhcp_server.sh

--- dhcp_server.sh
+++ dhcp_server.sh
@@ -1,8 +1,8 @@
-```console
+#Pre-requis
 apt-get update && apt-get install isc-dhcp-server -yy
-```
+
 # nano /etc/dhcp/dhcpd.conf 
-```{r, engine='bash', count_lines}
+
 ddns-update-style none;
 option domain-name "local.lan";
 option domain-name-servers 9.9.9.9, 1.1.1.1;
@@ -16,8 +16,8 @@ subnet 192.168.17.0 netmask 255.255.255.0 {
     option broadcast-address 192.168.17.255;
     option routers 192.168.17.254;
 }
-```
-```shell
+
+
 # Edition du fichier de configuration
 nano /etc/default/isc-dhcp-server
 # Contenu à modifier
