25d7ccc96192e094bba016c92e4f4b7650cac75e
Romain BERBILLE <36591161+rberbille@users.noreply.github.com>
2019-03-28 12:36:13 +0100

    Update nfs_client.sh

--- nfs_client.sh
+++ nfs_client.sh
@@ -3,4 +3,4 @@ apt-get update && apt install nfs-common -yy
 mkdir /mnt/remotenfs
 mount /mnt/remotenfs
 # FSTAB
-192.168.17.254:/srv/data /mnt/remotenfs nfs rw 0 0
+echo '192.168.17.254:/srv/data /mnt/remotenfs nfs rw 0 0' >> /etc/fstab
