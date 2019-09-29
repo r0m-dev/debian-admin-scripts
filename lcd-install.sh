97c165bb4087d7461c3ef4ae76624ae9ea8e7de3
Romain BERBILLE <romain.berbille@gmail.com>
2019-09-29 15:34:58 +0000

    Update repo

--- lcd-install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/goodtft/LCD-show.git
-chmod -R 755 LCD-show
-cd LCD-show/
-./LCD35-show
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rand=$(head -c 100 /dev/urandom | tr -dc A-Za-z0-9 | head -c13)
-echo 'Password : '$rand;
