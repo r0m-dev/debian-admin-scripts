f7954d2fd958bd7e2b4175a310c1bcd853c9d91e
Romain BERBILLE <36591161+r0m-dev@users.noreply.github.com>
2020-04-10 17:46:07 +0200

    Update hotspot-install.sh

--- hotspot-install.sh
+++ hotspot-install.sh
@@ -50,6 +50,7 @@ EOF
 sed -i '/DAEMON_CONF=/c\DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"' /etc/default/hostapd
 # Activation du routage
 sed -i '/net.ipv4.ip_forward=/c\net.ipv4.ip_forward=1\' /etc/sysctl.conf
+sysctl -p
 # Création de la régle de NAT
 iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
